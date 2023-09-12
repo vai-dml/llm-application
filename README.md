14b6e88bbf4815f1cb20b076fde9cae73882d5e1
Vaibhav <vaibhav@cldcvr.com>
2023-09-12 22:51:13 +0800

    Summarize news article based on gpt-4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # llm-building-using-activeloop
-HandsOn LLM applications using LangChain and Deep Lake.
+HandsOn LLM applications using LangChain and Deep Lake. Langchain gives developers a set of tools and components to build LLM powered applications.
 
 ## The Chains
 
@@ -46,4 +46,5 @@ The conversational-react-description agent is designed for conversational situat
 * occurrence of hallucinations - models can produce text that appears plausible on the surface but is actually factually incorrect or unrelated to the given input.
 * LLMs may exhibit biases originating from their training data, resulting in outputs that can generate undesired outcomes.
 * LLM examples include Text Summarization, Text Translation, and Question Answering
+* Well-known models like GPT family, LLaMA employ subword level tokenization method. One of the variant used is Byte Pair Encoding (BPE)
 * 
--- summarize_news_article.py
+++ summarize_news_article.py
@@ -1,1 +1,64 @@
+# Develop an application that scraps online articles 
+# and generate concice summary.
+import requests
+from newspaper import Article
+from langchain.schema import (
+    HumanMessage
+)
+from langchain.chat_models import ChatOpenAI
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+article_url = "https://www.artificialintelligence-news.com/2022/01/25/meta-claims-new-ai-supercomputer-will-set-records/"
+
+def scrape_parse_article(article_url):
+
+    session = requests.Session()
+    try:
+        response = session.get(article_url, headers=headers, timeout=10)
+        if response.status_code == 200:
+            article = Article(article_url)
+            article.download()
+            article.parse()
+    
+            return article.title, article.text
+        else:
+            print(f"Failed to fetch article at {article_url}")
+    except Exception as e:
+        print(f"Error occurred while fetching article at {article_url}: {e}")
+
+
+def generate_summary():
+
+    # we get the article data by calling the scraping function
+    article_title, article_text = scrape_parse_article(article_url)
+    print(f"Title: {article_title}")
+    print(f"Text: {article_text}") 
+
+    # prepare template for prompt
+    template = """"You are an advnaced assistant that summarizes online news articles into bullet points
+
+    Here's the article you want to summarize.
+
+    ==================
+    Title: {article_title}
+
+    {article_text}
+    ==================
+
+    Write a summary of the previous article.
+    """
+
+    prompt = template.format(article_title=article_title, article_text=article_text)
+    messages = [HumanMessage(content=prompt)]
+
+    # load the model
+    chat = ChatOpenAI(model_name="gpt-4", temperature=0)
+    
+    # generate summary
+    summary = chat(messages)
+    print(summary.content)
+
+
+generate_summary()
